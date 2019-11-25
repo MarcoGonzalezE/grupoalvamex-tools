fc1d83513a578765a52fd94f89776a476963d7f8
marcogonzaleze <marco.gonzalez.grupoalvamex@gmail.com>
2019-11-25 12:03:05 -0600

    Initial commit

--- __manifest__.py
+++ __manifest__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Avicampo Tools",
+    'name': "Palmito Tools",
 
     'summary': """
-        Herramientas Avicampo""",
+        Herramientas Palmito""",
 
     'description': """
         Contabilidad
