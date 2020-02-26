26cad40bc677a1f7a39a0677479e30f2f5b5572d
marcogonzaleze <marco.gonzalez.grupoalvamex@gmail.com>
2020-02-26 16:08:17 -0600

    Notificacion Almacen - Compras

--- models/almacen_general.py
+++ models/almacen_general.py
@@ -7,6 +7,34 @@ class PurchaseOrderAlmacenGeneral(models.Model):
     _inherit = 'purchase.order'
     _description = 'Recepcion de material en almacen general'
 
-
-    received = fields.Selection([('completed_received','Recibido Completo'),('partial_received','Recibido Incompleto'),('pending','Pendiente de Recibir')],string='Status Almacen General',default='pending')
+    received = fields.Selection([('completed_received', 'Recibido Completo'), ('partial_received', 'Recibido Incompleto'), ('pending', 'Pendiente de Recibir')], string='Status Almacen General', default='pending')
     received_date = fields.Date('Fecha Recibido')
+
+    @api.onchange('received')
+    def notificacion(self):
+        print("ENTRADO A NOTIFICACION")
+        compra = self.env['purchase.order'].search([('name', '=', self.name)])
+        followers = self.env['mail.followers'].search(
+            [('res_model', '=', 'purchase.order'), ('res_id', '=', compra.id)])
+        if self.received == 'completed_received' or self.received == 'partial_received':
+            print("Entrando condicion - Recibido Completo")
+            print(compra.id)
+            for follow in followers:
+                print(follow.partner_id.name)
+                notificacion_template = self.env['ir.model.data'].sudo().get_object('grupoalvamex_tools', 'notificacion_compra_almacen')
+                values = notificacion_template.generate_email(compra.id)
+                values['model'] = 'purchase.order'
+                values['res_id'] = self.id  # OJO AQUI
+                if self.received == 'completed_received':
+                	values['body_html'] = values['body_html'].replace("_estado_compra_", "Recibido Completo")
+                if self.received == 'partial_received':
+                	values['body_html'] = values['body_html'].replace("_estado_compra_", "Recibido Incompleto")
+                values['email_to'] = follow.partner_id.email
+                send_mail = self.env['mail.mail'].create(values)
+                send_mail.send()
+
+# class AlmacenGeneralEstados(model.Model):
+# 	_name = 'almacen.general.estados'
+
+# 	name = field.Char(required=True, string="Status Almacen General")
+# 	mail_template_id = field.Many2one('mail.template', domain="[('model_id','=','purchase.order')]", string="Mail Template")
