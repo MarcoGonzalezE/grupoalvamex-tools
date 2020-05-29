69ccb2be6c65fc16cb39de677efe3e859595f5d9
marcogonzaleze <marco.gonzalez.grupoalvamex@gmail.com>
2020-05-29 16:08:38 -0500

    Importacion Lineas de Pedidos de Venta

--- wizard/importar_datos.py
+++ wizard/importar_datos.py
@@ -10,6 +10,7 @@ import io as StringIO
 import xlrd
 from odoo.tools import ustr
 
+#COMPRAS
 class purchase_order(models.Model):
     _inherit = "purchase.order"
     
@@ -339,7 +340,340 @@ class ImportarLineasComprasWizard(models.TransientModel):
                     res = self.show_success_msg(completed_records, skipped_line_no)
                     return res
 
+#VENTAS
+class sale_order(models.Model):
+    _inherit = "sale.order"
+    
+    @api.multi
+    def importar_ventas(self):
+        if self:
+            action = self.env.ref('grupoalvamex_tools.sh_import_sol_action').read()[0]
+            return action
 
+class ImportarLineasVentasWizard(models.TransientModel):
+    _name="import.sol.wizard"
+
+    import_type = fields.Selection([
+        ('csv','CSV'),
+        ('excel','Excel')
+        ],default="excel",string="Tipo de Importacion",required=True)
+    file = fields.Binary(string="Archivo",required=True)   
+    product_by = fields.Selection([
+        ('name','Nombre'),
+        ('int_ref','Referencia Interna'),
+        ('barcode','Codigo de Barras')
+        ],default="name", string = "Buscar producto por", required = True) 
+
+    @api.multi
+    def show_success_msg(self,counter,skipped_line_no):
+        
+        #to close the current active wizard        
+        action = self.env.ref('grupoalvamex_tools.sh_import_sol_action').read()[0]
+        action = {'type': 'ir.actions.act_window_close'} 
+        
+        #open the new success message box    
+        view = self.env.ref('grupoalvamex_tools.sh_message_wizard')
+        view_id = view and view.id or False                                   
+        context = dict(self._context or {})
+        dic_msg = str(counter) + " Registros importados exitosamente"
+        if skipped_line_no:
+            dic_msg = dic_msg + "\nNota:"
+        for k,v in skipped_line_no.items():
+            dic_msg = dic_msg + "\nFila N. " + k + " " + v + " "
+        context['message'] = dic_msg            
+        
+        return {
+            'name': 'Informacion',
+            'type': 'ir.actions.act_window',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'sh.message.wizard',
+            'views': [(view.id, 'form')],
+            'view_id': view.id,
+            'target': 'new',
+            'context': context,
+            }   
+
+    
+    @api.multi
+    def import_sol_apply(self):
+        sol_obj = self.env['sale.order.line']
+        #perform import lead
+        if self and self.file and self.env.context.get('sh_so_id',False):
+            #For CSV
+            if self.import_type == 'csv':
+                counter = 1
+                skipped_line_no = {}
+                try:
+                    file = str(base64.decodestring(self.file).decode('utf-8'))
+                    myreader = csv.reader(file.splitlines())
+                    skip_header=True
+                     
+                    for row in myreader:
+                        try:
+                            if skip_header:
+                                skip_header=False
+                                counter = counter + 1
+                                continue
+
+                            if row[0] != '': 
+                                vals={}
+                                
+                                field_nm = 'name'
+                                if self.product_by == 'name':
+                                    field_nm = 'name'
+                                elif self.product_by == 'int_ref':
+                                    field_nm = 'default_code'
+                                elif self.product_by == 'barcode':
+                                    field_nm = 'barcode'
+                                
+                                search_product = self.env['product.product'].search([(field_nm,'=',row[0])], limit = 1)
+                                if search_product:
+                                    vals.update({'product_id' : search_product.id})
+
+                                    if row[1] in (None, ""):
+                                        continue
+                                    else:
+                                        search_lote = self.env['stock.picking.batch'].search([('name','=',row[1])], limit = 1)
+                                        if search_lote:
+                                            vals.update({'batch_id' : search_lote.id})
+                                        else:
+                                            skipped_line_no[str(counter)]= " - Lote no encontrada. "
+                                            counter = counter + 1
+                                            continue
+
+                                    if row[2] != '':
+                                        vals.update({'kilogram_price' : row[2] })
+
+                                    if row[3] != '':
+                                        vals.update({'kilograms' : row[3] })
+
+                                    if row[4] != '':
+                                        vals.update({'weighing_ticket' : row[4] })
+
+                                    if row[5] in (None, ""):
+                                        continue
+                                    else:
+                                        search_almacen = self.env['stock.warehouse'].search([('name', '=', row[5])], limit = 1)
+                                        if search_almacen:
+                                            vals.update({'warehouse_id' : search_almacen.id})
+                                        else:
+                                            skipped_line_no[str(counter)]= " - Almacen no encontrada. "
+                                            counter = counter + 1
+                                            continue                                    
+                                    
+                                    if row[6] != '':
+                                        vals.update({'name' : row[6] })
+                                    else:
+                                        vals.update({'name' : search_product.name})
+                                        
+                                    if row[7] != '':
+                                        vals.update({'product_uom_qty' : row[7] })
+                                    else:
+                                        vals.update({'product_uom_qty' : 1 })
+                                    
+                                    if row[8] in (None,"") and search_product.uom_id:
+                                        vals.update({'product_uom' : search_product.uom_id.id })
+                                    else:
+                                        search_uom = self.env['product.uom'].search([('name','=',row[8] )], limit = 1) 
+                                        if search_uom:
+                                            vals.update({'product_uom' : search_uom.id })
+                                        else:
+                                            skipped_line_no[str(counter)]= " - Unidad de Medidad no encontrada. " 
+                                            counter = counter + 1
+                                            continue
+                                    
+                                    if row[9] in (None,""):
+                                        vals.update({'price_unit' : search_product.lst_price })
+                                    else:
+                                        vals.update({'price_unit' : row[9] })
+                                        
+                                    if row[10].strip() in (None,"") and search_product.taxes_id:
+                                        vals.update({'tax_id' : [(6, 0, search_product.taxes_id.ids)]})
+                                    else:
+                                        taxes_list = []
+                                        some_taxes_not_found = False
+                                        for x in row[10].split(','):
+                                            x = x.strip()
+                                            if x != '':
+                                                search_tax = self.env['account.tax'].search([('name','=',x)], limit = 1)
+                                                if search_tax:
+                                                    taxes_list.append(search_tax.id)
+                                                else:
+                                                    some_taxes_not_found = True
+                                                    skipped_line_no[str(counter)]= " - Impuesto " + x +  " no encontrado. "                                                 
+                                                    break  
+                                        if some_taxes_not_found:
+                                            counter = counter + 1
+                                            continue
+                                        else:
+                                            vals.update({'tax_id' : [(6, 0, taxes_list)]})
+                                        
+                                else:
+                                    skipped_line_no[str(counter)]= " - Producto no encontrado. " 
+                                    counter = counter + 1 
+                                    continue
+                                
+                                vals.update({'order_id' : self.env.context.get('sh_so_id')})
+                                
+                                created_sol = sol_obj.create(vals)
+                                counter = counter + 1
+                            
+                            else:
+                                skipped_line_no[str(counter)] = " - Product is empty. "  
+                                counter = counter + 1      
+                        
+                        except Exception as e:
+                            skipped_line_no[str(counter)] = " - El valor no es valido " + ustr(e)   
+                            counter = counter + 1 
+                            continue          
+                             
+                except Exception:
+                    raise UserError(_("Lo sentimos, su archivo csv no coincide con nuestro formato"))
+                 
+                if counter > 1:
+                    completed_records = (counter - len(skipped_line_no)) - 2
+                    res = self.show_success_msg(completed_records, skipped_line_no)
+                    return res
+ 
+             
+            #For Excel
+            if self.import_type == 'excel':
+                counter = 1
+                skipped_line_no = {}                  
+                try:
+                    wb = xlrd.open_workbook(file_contents=base64.decodestring(self.file))
+                    sheet = wb.sheet_by_index(0)     
+                    skip_header = True    
+                    for row in range(sheet.nrows):
+                        try:
+                            if skip_header:
+                                skip_header = False
+                                counter = counter + 1
+                                continue
+                            
+                            if sheet.cell(row,0).value != '': 
+                                vals={}
+                                
+                                field_nm = 'name'
+                                if self.product_by == 'name':
+                                    field_nm = 'name'
+                                elif self.product_by == 'int_ref':
+                                    field_nm = 'default_code'
+                                elif self.product_by == 'barcode':
+                                    field_nm = 'barcode'
+
+                                search_product = self.env['product.product'].search([(field_nm,'=',sheet.cell(row,0).value )], limit = 1)
+                                if search_product:
+                                    vals.update({'product_id' : search_product.id})
+
+                                    if sheet.cell(row,1).value in (None, ""):
+                                        continue
+                                    else:
+                                        search_lote = self.env['stock.picking.batch'].search([('name','=',sheet.cell(row,1).value)], limit = 1)
+                                        if search_lote:
+                                            vals.update({'batch_id' : search_lote.id})
+                                        else:
+                                            skipped_line_no[str(counter)]= " - Lote no encontrada. "
+                                            counter = counter + 1
+                                            continue
+
+                                    if sheet.cell(row,2).value != '':
+                                        vals.update({'kilogram_price' : sheet.cell(row,2).value })
+
+                                    if sheet.cell(row,3).value != '':
+                                        vals.update({'kilograms' : sheet.cell(row,3).value })
+
+                                    if sheet.cell(row,4).value != '':
+                                        vals.update({'weighing_ticket' : sheet.cell(row,4).value })
+
+                                    if sheet.cell(row,5).value in (None, ""):
+                                        continue
+                                    else:
+                                        search_almacen = self.env['stock.warehouse'].search([('name', '=', sheet.cell(row,5).value)], limit = 1)
+                                        if search_almacen:
+                                            vals.update({'warehouse_id' : search_almacen.id})
+                                        else:
+                                            skipped_line_no[str(counter)]= " - Almacen no encontrada. "
+                                            counter = counter + 1
+                                            continue                                    
+                                    
+                                    if sheet.cell(row,6).value != '':
+                                        vals.update({'name' : sheet.cell(row,6).value })
+                                    else:
+                                        vals.update({'name' : ('['+search_product.default_code+'] '+search_product.name)})
+                                        
+                                    if sheet.cell(row,7).value != '':
+                                        vals.update({'product_uom_qty' : sheet.cell(row,7).value })
+                                    else:
+                                        vals.update({'product_uom_qty' : 1 })
+                                    
+                                    if sheet.cell(row,8).value in (None,"") and search_product.uom_id:
+                                        vals.update({'product_uom' : search_product.uom_id.id })
+                                    else:
+                                        search_uom = self.env['product.uom'].search([('name','=',sheet.cell(row,8).value )], limit = 1) 
+                                        if search_uom:
+                                            vals.update({'product_uom' : search_uom.id })
+                                        else:
+                                            skipped_line_no[str(counter)]= " - Unidad de Medidad no encontrada. " 
+                                            counter = counter + 1
+                                            continue
+                                    
+                                    if sheet.cell(row,9).value in (None,""):
+                                        vals.update({'price_unit' : search_product.lst_price })
+                                    else:
+                                        vals.update({'price_unit' : sheet.cell(row,9).value })
+                                        
+                                    if sheet.cell(row,10).value.strip() in (None,"") and search_product.taxes_id:
+                                        vals.update({'tax_id' : [(6, 0, search_product.taxes_id.ids)]})
+                                    else:
+                                        taxes_list = []
+                                        some_taxes_not_found = False
+                                        for x in sheet.cell(row,10).value.split(','):
+                                            x = x.strip()
+                                            if x != '':
+                                                search_tax = self.env['account.tax'].search([('name','=',x)], limit = 1)
+                                                if search_tax:
+                                                    taxes_list.append(search_tax.id)
+                                                else:
+                                                    some_taxes_not_found = True
+                                                    skipped_line_no[str(counter)]= " - Impuesto " + x +  " no encontrado. "                                                 
+                                                    break  
+                                        if some_taxes_not_found:
+                                            counter = counter + 1
+                                            continue
+                                        else:
+                                            vals.update({'tax_id' : [(6, 0, taxes_list)]})
+                                        
+                                else:
+                                    skipped_line_no[str(counter)]= " - Producto no encontrado. " 
+                                    counter = counter + 1 
+                                    continue
+                                
+                                vals.update({'order_id' : self.env.context.get('sh_so_id')})
+                                
+                                created_sol = sol_obj.create(vals)
+                                counter = counter + 1
+                            
+                            else:
+                                skipped_line_no[str(counter)] = " - Product is empty. "  
+                                counter = counter + 1                                  
+                        
+                        except Exception as e:
+                            skipped_line_no[str(counter)] = " - El valor no es valido " + ustr(e)   
+                            counter = counter + 1 
+                            continue          
+                             
+                except Exception:
+                    raise UserError(_("Lo sentimos, su archivo de excel no coincide con nuestro formato"))
+                 
+                if counter > 1:
+                    completed_records = (counter - len(skipped_line_no)) - 2
+                    res = self.show_success_msg(completed_records, skipped_line_no)
+                    return res
+
+
+#MENSAJE
 class sh_message_wizard(models.TransientModel):
     _name="sh.message.wizard"
     
