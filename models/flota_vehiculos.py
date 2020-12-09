62131d6d465bea3b61711379fa952e9d6c31a996
marcogonzaleze <marco.gonzalez.grupoalvamex@gmail.com>
2020-12-09 17:57:05 -0600

    Asignacion de Licencia por Operador

--- models/flota_vehiculos.py
+++ models/flota_vehiculos.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import fields, models, api, _
+from datetime import datetime
 
 class FleetVehicle(models.Model):
 	_inherit = 'fleet.vehicle'
@@ -10,6 +11,12 @@ class FleetVehicle(models.Model):
 	assign_id = fields.Many2one('fleet.vehicle.assignments', string="Asignacion ID")
 	state_id = fields.Many2one()
 	driver_id = fields.Many2one()
+	employee_id = fields.Many2one('hr.employee', string="Conductor")
+	license_state = fields.Selection([('activa', 'Licencia Vigente'), 
+		('inactiva', 'Licencia Expirada'), 
+		('pendiente', 'Licencia cerca de Expirar')], 
+		string='Estado Licencia de Conductor', compute="_state_license")
+
 
 	@api.multi
 	def assign_vehicle(self):
@@ -33,7 +40,7 @@ class FleetVehicle(models.Model):
 		form_id = self.env.ref('grupoalvamex_tools.fleet_vehicle_return_view_form')
 		state = self.env['fleet.vehicle.state'].search([('name','=','Disponible')]).id
 		self.state_id = state
-		self.driver_id = False
+		self.employee_id = False
 		self.date_assign = False
 		return{
 			'name': "Devolucion de Vehiculo",
@@ -48,16 +55,29 @@ class FleetVehicle(models.Model):
 
 	@api.onchange('assign_id')
 	def onchange_state(self):
-		self.driver_id = self.assign_id.driver_id
+		self.employee_id = self.assign_id.employee_id
 		self.date_assign = self.assign_id.date_assign
+
+	@api.depends('employee_id')
+	def _state_license(self):
+		for r in self:
+			if r.employee_id:
+				if r.employee_id.days_expire == 0:
+					r.license_state = 'inactiva'
+				if r.employee_id.days_expire > 0 and r.employee_id.days_expire < 21:
+					r.license_state = 'pendiente'
+				if r.employee_id.days_expire > 20:
+					r.license_state = 'activa'
 
 
 
 class FleetVehicleAssignments(models.Model):
 	_name = 'fleet.vehicle.assignments'
+	_rec_name = "employee_id"
 
 	vehicle_id = fields.Many2one('fleet.vehicle', string="Vehiculo")
 	driver_id = fields.Many2one('res.partner', string="Conductor")
+	employee_id = fields.Many2one('hr.employee', string="Conductor")
 	driver_job = fields.Char(string="Puesto de Trabajo")
 	date_assign = fields.Date(string="Fecha de Asignacion")
 	date_return = fields.Date(string="Fecha de Devolucion")
@@ -66,9 +86,29 @@ class FleetVehicleAssignments(models.Model):
 	def assign_vehicle(self):
 		assignments = self.env['fleet.vehicle.assignments'].search([],order='id desc')[0]
 		self.vehicle_id.assign_id = assignments.id
-		self.vehicle_id.driver_id = assignments.driver_id
+		self.vehicle_id.employee_id = assignments.employee_id
 		self.vehicle_id.date_assign = assignments.date_assign
 
 	@api.multi
 	def save(self):
 		return True
+
+class HrEmployee(models.Model):
+	_inherit = 'hr.employee'
+
+	driver = fields.Boolean(string="Operador")
+	driver_license = fields.Char(string="ID de Licencia")
+	license_type = fields.Char(string="Tipo de Licencia")
+	license_valid = fields.Date(string="Licencia Valida Desde")
+	license_expiration = fields.Date(string="Fecha de Expiracion")
+	days_expire = fields.Integer(string="Dias para Expirar", compute="_compute_days_to_expire")
+
+	@api.depends('license_expiration')
+	def _compute_days_to_expire(self):
+		for r in self:
+			date = datetime.now()
+			if r.license_expiration:
+				date = datetime.strptime(r.license_expiration, '%Y-%m-%d')
+			now = datetime.now()
+			delta = date - now
+			r.days_expire = delta.days if delta.days > 0 else 0
